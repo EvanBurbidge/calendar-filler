555e970e2d716a8c80fe4f6e4185424a3d0c01e0
EvanBurbidge <burbidge@tcd.ie>
2023-03-15 11:34:06 +0000

    remove dayjs

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const dayjs = require('dayjs');
-
-
 const gridSize = 42;
 
-// interface CalendarDay {
-//   date: string,
-//   day;
-// }
+const getDaysInMonth = (year, month) => new Date(year, month, 0).getDate();
+
+const getFormattedMonth = (month) => {
+  const monthToUse = month + 1;
+  if (monthToUse < 10) {
+    return `0${monthToUse}`
+  }
+  return monthToUse
+}
+const getFormattedDay = (day) => {
+  const monthToUse = month + 1;
+  if (day < 10) {
+    return `0${day}`
+  }
+  return day
+}
 
 const generateDateArray = (dateToUse, month) => {
   const results = []
   while(dateToUse.getMonth() === month) {
     results.push({
-      date: dayjs.unix(dateToUse.getTime() / 1000).format("YYYY-MM-DD") ,
+      date: `${dateToUse.getYear()}-${getFormattedMonth(month)}-${getFormattedDay(dateToUse.getDate())}`,
       day: dateToUse.getDay()
     })
     dateToUse.setDate(dateToUse.getDate() + 1);
@@ -39,7 +48,7 @@ const getCorrectMonthAndYear = (year, month) => {
 
 const padStartDate = (year, month, upTo) => {
   const { monthToUse, yearToUse } = getCorrectMonthAndYear(year, month);
-  const daysInMonth = dayjs.unix(new Date(yearToUse, monthToUse, 1).getTime() / 1000).daysInMonth();
+  const daysInMonth = getDaysInMonth(yearToUse, monthToUse);
   const startDate = new Date(yearToUse, monthToUse, daysInMonth - upTo);
   while(startDate.getDay() !== 0) {
     startDate.setDate(startDate.getDate() + 1);
